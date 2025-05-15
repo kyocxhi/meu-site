a77ea8c5a7afec3ea63f3417ce2350a8757bbf44
kyocxhi <gomeswendell2004@gmail.com>
2025-05-15 00:00:20 -0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,32 @@
+const animes = [
+  {
+    id: 1,
+    title: "Naruto",
+    image: "https://i.imgur.com/hsOasjJ.jpeg",
+    description: "Naruto é um jovem ninja que busca ser reconhecido e se tornar o Hokage.",
+    seasons: [
+      {
+        season: 1,
+        episodes: [
+          { number: 1, url: "https://www.youtube.com/embed/JrH4CH3z3_E" },
+          { number: 2, url: "https://www.youtube.com/embed/vnPbV6HqUuA" }
+        ]
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: "Hunter x Hunter",
+    image: "https://i.imgur.com/dfFOgpL.jpeg",
+    description: "A história de Gon Freecss, que busca encontrar seu pai e se tornar um Hunter.",
+    seasons: [
+      {
+        season: 1,
+        episodes: [
+          { number: 1, url: "https://www.youtube.com/embed/mF3sH1VGgys" },
+          { number: 2, url: "https://www.youtube.com/embed/mF3sH1VGgys" }
+        ]
+      }
+    ]
+  }
+];
